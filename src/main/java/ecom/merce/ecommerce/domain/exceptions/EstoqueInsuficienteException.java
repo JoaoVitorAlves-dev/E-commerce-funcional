dd9c47053611c444d23e20586933d96a57f282fc
JoaoVitorAlves-dev <ytmarcos43@gmail.com>
2026-09-21 15:40:34 -0300

    feat: Adiciona EstoqueInsuficienteException

---
+++ src/main/java/ecom/merce/ecommerce/domain/exceptions/EstoqueInsuficienteException.java
@@ -0,0 +1,9 @@
+package ecom.merce.ecommerce.domain.exceptions;
+
+public class EstoqueInsuficienteException extends RuntimeException {
+
+    public EstoqueInsuficienteException(String message) {
+        super(message);
+    }
+
+}
